7a9daba8aa0674d17ce76f82882f7bb5c4a21994
Ray <imandingal@gmail.com>
2025-11-14 12:50:26 +0800

    modified the sidebar header and footer

--- src/app/components/main-layout/main-layout.ts
+++ src/app/components/main-layout/main-layout.ts
@@ -1,21 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { Header } from '../header/header';
 import { Footer } from '../footer/footer';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet, Header, Footer, CommonModule],
+  imports: [
+    RouterOutlet,
+    Header,
+    Footer,
+    CommonModule,
+    NgClass,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './main-layout.html',
-  styleUrls: ['./main-layout.scss']
+  styleUrls: ['./main-layout.scss'],
 })
 export class MainLayoutComponent {
-
-  sidebarOpen = true;
+  
+  sidebarOpen = true; 
+  isDesktopCollapsed = false;
 
   toggleSidebar() {
     this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  toggleDesktopSidebar() {
+    this.isDesktopCollapsed = !this.isDesktopCollapsed;
   }
 }
--- src/app/content/content.spec.ts
+++ src/app/content/content.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MainLayoutComponent } from './main-layout';
+import { Content } from './content';
 
-describe('MainLayout', () => {
-  let component: MainLayoutComponent;
-  let fixture: ComponentFixture<MainLayoutComponent>;
+describe('Content', () => {
+  let component: Content;
+  let fixture: ComponentFixture<Content>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MainLayoutComponent]
+      imports: [Content]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(MainLayoutComponent);
+    fixture = TestBed.createComponent(Content);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
